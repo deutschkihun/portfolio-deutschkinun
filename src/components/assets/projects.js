64bc4e794cd73fb67f897884e7789edd19505f16
Kihun Kim <kihunkim@Kihuns-MacBook-Pro.local>
2022-03-21 17:34:59 +0100

    update portfolio

--- src/components/assets/projects.js
+++ src/components/assets/projects.js
@@ -8,45 +8,42 @@ import img7 from "./img7.png";
 import img8 from "./img8.png";
 import img9 from "./img9.png";
 import img10 from "./img10.png";
-import img11 from "./img11.png";
-import img12 from "./img12.png";
-import img13 from "./img13.png";
 
 export const projects = [
   {
     id: "1",
-    title: "Kihun's portfolio web application",
-    description: "Current portfolio application (www.deutschkihun.com)",
+    title: "LUXE SCHMUCK",
+    description: "Luxeschmuck is a comprehensive luxury e-commerce platform.",
     img: img1,
-    link: "https://deutschkihun.com",
-    source: "https://github.com/deutschkihun/Portfolio-WebApp",
+    link: "https://luxeschmuck.herokuapp.com/",
+    source: "https://github.com/deutschkihun/LuxeSchmuck",
   },
   {
     id: "2",
-    title: "Login System Boilerplate",
+    title: "D3 & React & GSAP exhibition",
+    description:
+      "Data visualization in various forms in web browsers using javascript and React, HTML, CSS, and D3. D3 is a JavaScript library for manipulating documents based on data.",
     img: img2,
+    link: "https://d3-react-exhibition.herokuapp.com/",
+    source: "https://github.com/deutschkihun/D3-GSAP-React-Exhibition",
+  },
+  {
+    id: "3",
+    title: "Login System Boilerplate",
+    img: img3,
     description:
       "Design of a generic login system boilplate for web application. It contains sign-in,sign-up, find emial, find password, update account etc.",
     link: "https://login-boilerplate-deutschkihun.herokuapp.com/",
     source: "https://github.com/deutschkihun/LoginSystem-Boilerplate",
   },
   {
-    id: "3",
+    id: "4",
     title: "Online Sport DB Application",
     description:
       "Getting real-time updated sports teams and players information around the world ",
-    img: img3,
+    img: img4,
     link: "https://sportdbapp-deutschkihun.herokuapp.com/",
     source: "https://github.com/deutschkihun/SportDBApp",
-  },
-  {
-    id: "4",
-    title: "D3 & React & GSAP exhibition",
-    description:
-      "Getting real-time updated sports teams and players information around the world. This project is in progress",
-    img: img4,
-    link: "https://d3-react-exhibition.herokuapp.com/",
-    source: "https://github.com/deutschkihun/D3-GSAP-React-Exhibition",
   },
   {
     id: "5",
@@ -67,55 +64,33 @@ export const projects = [
   },
   {
     id: "7",
-    title: "User Preference Service",
+    title: "Digit recognition on blast furnace",
+    description:
+      "Seminar project: recognizing digit on blast furnace by AI automatically",
     img: img7,
+    paper: "/files/Seminar_Digitalisierung_in_der_Stahlindustrie.pdf",
+  },
+  {
+    id: "8",
+    title: "User Preference Service",
+    img: img8,
     description:
       "Bachelor thesis: building user preference application for individual mobility users ",
     link: "https://github.com/deutschkihun/User-Preference-Service",
     paper: "/files/Bachelorarbeit_KihunKim.pdf",
   },
   {
-    id: "8",
-    title: "Digit recognition on blast furnace",
-    description:
-      "Seminar project: recognizing digit on blast furnace by AI automatically",
-    img: img8,
-    paper: "/files/Seminar_Digitalisierung_in_der_Stahlindustrie.pdf",
-  },
-  {
     id: "9",
-    title: "Must Know React",
-    description: "React that beginner must know",
+    title: "Frontend-KnowHow",
+    description: "What to know for frontend",
     img: img9,
-    link: "https://github.com/deutschkihun/Must-Know-React-With-TypeScript",
+    link: "https://github.com/deutschkihun/Frontend-KnowHow",
   },
   {
     id: "10",
-    title: "Must Know JavaScript",
-    description: "JavaScript that beginner must know",
+    title: "Statistic-KnowHow",
+    description: "What to know for statistic",
     img: img10,
-    link: "https://github.com/deutschkihun/Must-Know-JavaScript",
-  },
-  {
-    id: "11",
-    title: "Must Know TypeScript",
-    description: "TypeScript that beginner must know",
-    img: img11,
-    link: "https://github.com/deutschkihun/Must-Know-TypeScript",
-  },
-  {
-    id: "12",
-    title: "Must Know TDD (Test Driven Development)",
-    description: "TDD that beginner must know",
-    img: img12,
-    link: "https://github.com/deutschkihun/Must-Know-TDD",
-  },
-  {
-    id: "13",
-    title: "REST API devlopment for authentication and like/dislike system",
-    description:
-      "REST API dev with python fast-api, postgresql, and docker. This is project is in progess",
-    img: img13,
-    link: "https://github.com/deutschkihun/Python-RESTAPI",
+    link: "https://github.com/deutschkihun/Statistic-KnowHow",
   },
 ];
